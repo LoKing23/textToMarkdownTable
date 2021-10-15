1ac223ae5877717779006478f2b4a07193f75486
LoKing23 <mongrock30102@gmail.comm>
2021-10-15 17:31:17 +0800

    半成品

--- all.js
+++ all.js
@@ -1,18 +1,12 @@
 const submit = document.querySelector('.btn-success');
-const areas = document.querySelectorAll('.row textarea');
-console.log(submit);
-const data = [];
-let newData = ``;
-submit.addEventListener('click',()=>{
-  console.log(data);
-  if(data.length) return;
-  areas.forEach((textarea) => {
-    data.push(textarea.value.trim());
-  })
-  data.forEach((value, index, arr) => {
-    arr[index] = value.split('\n');
-  })
-  
+const areas = document.querySelectorAll('.input-area textarea');
+const output = document.querySelector('.output');
+
+// model
+function getMergeData(data){
+  let newData = `| 生字| 翻譯 | 
+| -------- | -------- | \n`;
+
   for(let i = 0;i < data[0].length;i++){
     newData += `|`;
     data.forEach((value) => {
@@ -20,6 +14,18 @@ submit.addEventListener('click',()=>{
     })
     newData += `\n`;
   }
-  console.log(data);
-  console.log(newData);
+  return newData;
+}
+//event
+submit.addEventListener('click',()=>{
+  const data = [];
+  areas.forEach((textarea) => {
+    data.push(textarea.value.trim());
+  })
+  data.forEach((value, index, arr) => {
+    arr[index] = value.split('\n');
+  })
+  const newData = getMergeData(data);
+  output.value = newData;
+
 })
